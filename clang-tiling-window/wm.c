6df48f3661c7b5e9eca221da07d4ba6872d5911b
zeindevs <zeindevs@gmail.com>
2024-08-07 19:27:33 +0700

    add: clang-tiling-window

---
+++ clang-tiling-window/wm.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <windows.h>
+
+HHOOK hookHandle;
+
+void ctrlc(int sig)
+{
+    UnhookWindowsHookEx(hookHandle);
+    exit(0);
+}
+
+int main()
+{
+    HMODULE wmDll = LoadLibraryW(L"wm_dll");
+    FARPROC shellProc = GetProcAddress(wmDll, "ShellProc");
+
+    hookHandle = SetWindowsHookExW(WH_SHELL, shellProc, wmDll, 0);
+
+    signal(SIGINT, ctrlc);
+
+    for (;;)
+    {
+    }
+}
---
+++ clang-tiling-window/wm_dll.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+__declspec(dllexport) LRESULT CALLBACK ShellProc(int code, WPARAM wparam, LPARAM lparam)
+{
+    if (code == HSHELL_WINDOWCREATED || code == HSHELL_WINDOWDESTROYED)
+    {
+        TileWindows(NULL, MDITILE_VERTICAL, NULL, NULL, NULL);
+    }
+
+    return CallNextHookEx(NULL, code, wparam, lparam);
+}
